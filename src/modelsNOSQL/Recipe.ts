3b64512cddfa5b59c2a5e92b5a58f1e5aa533d3f
Eduardo Cortez Valente <130951926+EduardoCorVal@users.noreply.github.com>
2023-10-27 16:29:13 -0600

    Feature(recipe): Added. Docker created

---
+++ src/modelsNOSQL/Recipe.ts
@@ -0,0 +1,43 @@
+// * Recipe
+
+// user_id
+// product: id_product
+// time:
+// recipe_name:
+// recipe_text: str
+// total_calores:
+// total_protein: 
+// total_portion:
+// total_fat:
+// avg_cost:
+
+import { Schema, model, Types } from "mongoose";
+
+export interface IRecipe {
+    user_id: Types.ObjectId;
+    product: Types.ObjectId;
+    time: Date;
+    recipe_name: string;
+    recipe_text: string;
+    total_calores: number;
+    total_protein: number;
+    total_portion: number;
+    total_fat: number;
+    avg_cost: number;
+}
+
+export const userSchema = new Schema<IRecipe>({
+    user_id: { type: Schema.Types.ObjectId, required: true, ref: "User"},
+    product: { type: Schema.Types.ObjectId, required: true, ref: "Product"},
+    time: { type: Date, default: Date.now, required: true },
+    recipe_name: { type: String, required: true },
+    recipe_text: { type: String, required: true },
+    total_calores: { type: Number, required: true },
+    total_protein: { type: Number, required: true },
+    total_portion: { type: Number, required: true },
+    total_fat: { type: Number, required: true },
+    avg_cost: { type: Number, required: true },
+});
+
+export const RecipeModel = model<IRecipe>("Recipe", userSchema);
+export default IRecipe;
